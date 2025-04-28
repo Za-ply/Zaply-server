ed209a207e446f676be03209a8326e44fdeb66fe
seongho5356 <kjeng7897@gmail.com>
2025-04-29 03:00:30 +0900

    #5 Feat: Swagger Config 에 JWT Bearer 인증 스킴 및 SecurityRequirement를 추가한다

--- src/main/java/org/zapply/product/global/config/SwaggerConfig.java
+++ src/main/java/org/zapply/product/global/config/SwaggerConfig.java
@@ -1,25 +1,42 @@
 package org.zapply.product.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
 @OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(title = "Meet-up", description = "Zaply Swagger", version = "v1"),
+        info = @Info(title = "MeetUp API 명세서",
+                description = "MeetUp API 명세서",
+                version = "v1"
+        ),
         servers = {
                 @Server(url = "http://localhost:8080", description = "로컬 서버 URL"),
                 @Server(url = "https://zapply.site", description = "배포 서버 URL")
         }
 )
+
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components());
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Collections.singletonList(securityRequirement));
     }
 }
+
+
+

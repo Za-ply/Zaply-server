f208ca7e5ecfb268704646427dbbad63f349c333
seongho5356 <kjeng7897@gmail.com>
2025-05-02 19:13:00 +0900

    #13 Fix: Cors Origin을 수정한다.

--- src/main/java/org/zapply/product/global/config/SecurityConfig.java
+++ src/main/java/org/zapply/product/global/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));

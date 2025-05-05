21d92a75648ad5df6124b8316849fcd8d8fec96f
seongho5356 <kjeng7897@gmail.com>
2025-05-05 17:20:28 +0900

    #26 Refactor: 메세지 템플릿을 상수로 관리한다

--- src/main/java/org/zapply/product/global/coolSMS/SMSClient.java
+++ src/main/java/org/zapply/product/global/coolSMS/SMSClient.java
@@ -20,6 +20,8 @@ public class SMSClient {
     @Value("${cloud.ncp.sms.sender-number}") // 발신자 번호 주입
     private String fromNumber;
 
+    private static final String MESSAGE_TEMPLATE = "[Zaply] 본인확인 인증번호는 %s입니다.";
+
     DefaultMessageService messageService; // 메시지 서비스를 위한 객체
 
     @PostConstruct // 의존성 주입이 완료된 후 초기화를 수행하는 메서드
@@ -32,7 +34,9 @@ public class SMSClient {
         Message message = new Message(); // 새 메시지 객체 생성
         message.setFrom(fromNumber); // 발신자 번호 설정
         message.setTo(to); // 수신자 번호 설정
-        message.setText("[Zaply] 본인확인 인증번호는 " + certificationCode + "입니다."); // 메시지 내용 설정
+
+        String text = String.format(MESSAGE_TEMPLATE, certificationCode);
+        message.setText(text);
 
         this.messageService.sendOne(new SingleMessageSendingRequest(message)); // 메시지 발송 요청
     }

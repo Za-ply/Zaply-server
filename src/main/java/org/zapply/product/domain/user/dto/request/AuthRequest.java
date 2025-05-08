76fbe0ec013a48f2e9799c73ba73b11dbc228f5a
yumzen <yeom.ji.eun35@gmail.com>
2025-05-09 02:46:16 +0900

    #32 Fix: 이름 필드 추가, 스키마 description 통일

--- src/main/java/org/zapply/product/domain/user/dto/request/AuthRequest.java
+++ src/main/java/org/zapply/product/domain/user/dto/request/AuthRequest.java
@@ -8,10 +8,13 @@ import org.zapply.product.domain.user.entity.Member;
 @Builder
 public record AuthRequest(
 
+        @Schema(description = "이름", example = "홍길동")
+        String name,
+
         @Schema(description = "이메일", example = "zaply123@gmail.com")
         String email,
 
-        @Schema(description = "전화번호", example = "010-1234-5678")
+        @Schema(description = "휴대폰 번호", example = "010-1234-5678")
         String phoneNumber,
 
         @Schema(description = "주민등록번호", example = "123456-1234567")
@@ -22,6 +25,7 @@ public record AuthRequest(
 ) {
     public Member toMember(Credential credential) {
         return Member.builder()
+                .name(name)
                 .email(email)
                 .phoneNumber(phoneNumber)
                 .residentNumber(residentNumber)

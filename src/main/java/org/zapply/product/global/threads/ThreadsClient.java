58659163084c6b89865dd772b735f647eef578f0
yumzen <yeom.ji.eun35@gmail.com>
2025-05-13 01:26:58 +0900

    Fix: thread 패키지 수정

--- src/main/java/org/zapply/product/global/threads/ThreadsClient.java
+++ src/main/java/org/zapply/product/global/threads/ThreadsClient.java
@@ -1,4 +1,4 @@
-package org.zapply.product.global.security.threads;
+package org.zapply.product.global.threads;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;

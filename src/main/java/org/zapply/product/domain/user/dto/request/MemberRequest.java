64ef5b287b76394462ab99f1065500ca6f81ad53
yumzen <yeom.ji.eun35@gmail.com>
2025-05-20 20:00:41 +0900

    Fix: JsonInclude 어노테이션 삭제

--- src/main/java/org/zapply/product/domain/user/dto/request/MemberRequest.java
+++ src/main/java/org/zapply/product/domain/user/dto/request/MemberRequest.java
@@ -1,8 +1,5 @@
 package org.zapply.product.domain.user.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MemberRequest(
         String name,
         String password

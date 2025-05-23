1a99ea8a06c90d583ff256d6fbe225b93755839c
yumzen <yeom.ji.eun35@gmail.com>
2025-05-24 00:11:28 +0900

    #94 Feat: 인스타그램 게시글 즉시 발행

--- src/main/java/org/zapply/product/domain/posting/service/InstagramPostingService.java
+++ src/main/java/org/zapply/product/domain/posting/service/InstagramPostingService.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.zapply.product.domain.user.entity.Account;
 import org.zapply.product.domain.user.entity.Member;
 import org.zapply.product.global.snsClients.instagram.InstagramPostingClient;
 import org.zapply.product.global.snsClients.instagram.InstagramPostingRequest;
+import org.zapply.product.global.snsClients.instagram.InstagramPostingResponse;
 
 import java.util.List;
 
@@ -19,14 +19,29 @@ public class InstagramPostingService {
 
     private final InstagramPostingClient instagramPostingClient;
 
+    /**
+     * 인스타그램 단일 이미지 게시
+     *
+     * @param member    사용자 정보
+     * @param request   게시물 요청 정보
+     * @param projectId 프로젝트 ID
+     * @return InstagramPostingResponse
+     */
     @Transactional
-    public String publishInstagramPost(Member member, InstagramPostingRequest request, Long postingId) {
-        return instagramPostingClient.createSingleMedia(member, request, postingId);
-        //instagramPostingClient.publishMediaContainer(member, creationId);
+    public InstagramPostingResponse publishInstagramPost(Member member, InstagramPostingRequest request, Long projectId) {
+        return instagramPostingClient.createSingleMedia(member, request, projectId);
     }
 
+    /**
+     * 인스타그램 캐러셀 게시
+     *
+     * @param member    사용자 정보
+     * @param request   게시물 요청 정보
+     * @param projectId 프로젝트 ID
+     * @return InstagramPostingResponse
+     */
     @Transactional
-    public String publishInstagramCarousel(Member member, InstagramPostingRequest requests, Long postingId) {
-        return instagramPostingClient.createCarouselMedia(member, requests.mediaUrls(), postingId);
+    public InstagramPostingResponse publishInstagramCarousel(Member member, InstagramPostingRequest request, Long projectId) {
+        return instagramPostingClient.createCarouselMedia(member, request.mediaUrls(), request.caption(), projectId);
     }
 }
--- src/main/java/org/zapply/product/global/snsClients/instagram/InstagramPostingClient.java
+++ src/main/java/org/zapply/product/global/snsClients/instagram/InstagramPostingClient.java
@@ -1,12 +1,17 @@
 package org.zapply.product.global.snsClients.instagram;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.zapply.product.domain.posting.entity.Posting;
+import org.zapply.product.domain.posting.enumerate.PostingState;
 import org.zapply.product.domain.posting.repository.PostingRepository;
+import org.zapply.product.domain.project.entity.Project;
 import org.zapply.product.domain.project.repository.ProjectRepository;
 import org.zapply.product.domain.user.entity.Account;
 import org.zapply.product.domain.user.entity.Member;
@@ -16,7 +21,6 @@ import org.zapply.product.global.apiPayload.exception.CoreException;
 import org.zapply.product.global.apiPayload.exception.GlobalErrorType;
 import org.zapply.product.global.clova.enuermerate.SNSType;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +39,7 @@ public class InstagramPostingClient {
 
     private final RestClient restClient = RestClient.create();
     private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     /**
      * Instagram 계정 불러오기
@@ -101,22 +106,6 @@ public class InstagramPostingClient {
     }
 
     /**
-     * Instagram에 미디어 만들기
-     *
-     * @param member
-     * @param request
-     * @param postingId
-     */
-    public String createSingleMedia(Member member, InstagramPostingRequest request, Long postingId) {
-        Account account = getInstagramAccount(member);
-        String accessToken = getAccessToken(member);
-
-        String mediaContainerId = createSingleMediaContainer(account.getUserId(), accessToken, request.mediaUrls().get(0), request.caption());
-
-        return mediaContainerId;
-    }
-
-    /**
      * Instagram 캐러셀 컨테이너 생성하기
      *
      * @param userId      Instagram User ID
@@ -163,27 +152,6 @@ public class InstagramPostingClient {
     }
 
     /**
-     * Instagram에 캐러셀 미디어 만들기
-     *
-     * @param member
-     * @param mediaUrls
-     * @param postingId
-     */
-    public String createCarouselMedia(Member member, List<String> mediaUrls, Long postingId) {
-        Account account = getInstagramAccount(member);
-        String accessToken = getAccessToken(member);
-
-        // 미디어 컨테이너 생성
-        List<String> mediaContainerIds = mediaUrls.stream()
-                    .map(url -> createSingleMediaContainer(account.getUserId(), accessToken, url, null))
-                    .toList();
-        // 캐러셀 컨테이너 생성
-        String carouselContainerId = createCarouselContainer(account.getUserId(), accessToken, null, mediaContainerIds);
-
-        return carouselContainerId;
-    }
-
-    /**
      * Instagram에 미디어 컨테이너를 게시하기
      *
      * @param userId        Instagram User ID
@@ -208,18 +176,121 @@ public class InstagramPostingClient {
                     .body(body)
                     .retrieve()
                     .body(String.class);
-
             Map<String, Object> response = objectMapper.readValue(rawResponse, Map.class);
-
             log.info("Instagram Media Publish Response: {}", response);
-            if (response == null || !response.containsKey("id")) {
+
+            if (response != null && response.containsKey("id")) {
+                return response.get("id").toString();
+            } else {
                 throw new CoreException(GlobalErrorType.INSTAGRAM_API_ERROR);
             }
 
-            return response.get("id").toString(); // 게시된 Media ID 반환
         } catch (Exception e) {
-            log.error("Error publishing media container: {}", e.getMessage(), e);
+            log.error("Unexpected error publishing media container: {}", e.getMessage(), e);
             throw new CoreException(GlobalErrorType.INSTAGRAM_API_ERROR);
         }
     }
+
+    /** 인스타그램 게시물 저장하기
+     * @param mediaId 인스타그램 미디어 ID
+     * @param projectId 프로젝트 ID
+     * @param mediaType 미디어 타입
+     * @param media 미디어 URL
+     * @param text 게시물 내용
+     * @return 게시물 ID
+     */
+    private InstagramPostingResponse savePosting(String mediaId, Long projectId, String mediaType, String media, String text) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new CoreException(GlobalErrorType.PROJECT_NOT_FOUND));
+        Posting posting = Posting.builder()
+                .postingLink("")
+                .postingType(SNSType.THREADS)
+                .postingState(PostingState.POSTED)
+                .project(project)
+                .mediaId(mediaId)
+                .build();
+        postingRepository.save(posting);
+        return InstagramPostingResponse.of(posting, mediaType, media, text);
+    }
+
+    /**
+     * Instagram에 미디어 만들기
+     *
+     * @param member
+     * @param request
+     * @param projectId
+     */
+    public InstagramPostingResponse createSingleMedia(Member member, InstagramPostingRequest request, Long projectId)  {
+        Account account = getInstagramAccount(member);
+        String accessToken = getAccessToken(member);
+
+        String mediaContainerId = createSingleMediaContainer(account.getUserId(), accessToken, request.mediaUrls().get(0), request.caption());
+
+        // 미디어 컨테이너 게시하기
+        String publishedId = publishMediaContainer(account.getUserId(), accessToken, mediaContainerId);
+
+        // 게시글 url 가져오기
+        String postingUrl = getPostingUrl(account.getUserId(), publishedId);
+
+        return savePosting(publishedId, projectId, "IMAGE", postingUrl, request.caption());
+    }
+
+    /**
+     * Instagram 게시물 URL 가져오기
+     * @param mediaId
+     * @param accessToken
+     * @return 게시물 URL
+     */
+    public String getPostingUrl(String mediaId, String accessToken){
+        try {
+            String uri = UriComponentsBuilder.fromHttpUrl(INSTAGRAM_API_BASE)
+                    .pathSegment("/{mediaId}", mediaId)
+                    .queryParam("fields", "id,media_type,media_url,owner,timestamp,caption,permalink,username")
+                    .queryParam("access_token", accessToken)
+                    .toUriString();
+
+            String response = restClient.get()
+                    .uri(uri)
+                    .retrieve()
+                    .body(String.class);
+
+            Map<String, Object> responseMap = objectMapper.readValue(response, Map.class);
+            return String.valueOf(responseMap.get("permalink"));
+        } catch (JsonProcessingException e) {
+            log.error("Error parsing JSON response: {}", e.getMessage());
+            throw new CoreException(GlobalErrorType.INSTAGRAM_API_ERROR);
+        } catch (Exception e) {
+            log.error("Error getting posting URL: {}", e.getMessage());
+            throw new CoreException(GlobalErrorType.INSTAGRAM_API_ERROR);
+        }
+    }
+
+    /**
+     * Instagram에 캐러셀 미디어 만들기
+     *
+     * @param member
+     * @param mediaUrls
+     * @param caption
+     * @param projectId
+     */
+    public InstagramPostingResponse createCarouselMedia(Member member, List<String> mediaUrls, String caption, Long projectId) {
+        Account account = getInstagramAccount(member);
+        String accessToken = getAccessToken(member);
+
+        // 미디어 컨테이너 생성
+        List<String> mediaContainerIds = mediaUrls.stream()
+                .map(url -> createSingleMediaContainer(account.getUserId(), accessToken, url, null))
+                .toList();
+        // 캐러셀 컨테이너 생성
+        String carouselContainerId = createCarouselContainer(account.getUserId(), accessToken, caption, mediaContainerIds);
+
+        // 캐러셀 컨테이너 게시하기
+        String publishedId = publishMediaContainer(account.getUserId(), accessToken, carouselContainerId);
+
+        // 게시글 url 가져오기
+        String postingUrl = getPostingUrl(account.getUserId(), publishedId);
+
+        return savePosting(publishedId, projectId, "CAROUSEL", postingUrl, caption);
+    }
+
 }

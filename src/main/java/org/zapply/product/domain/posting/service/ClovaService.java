5688f6d2b53aec74449a90067c296d84474949dc
yumzen <yeom.ji.eun35@gmail.com>
2025-05-24 21:26:07 +0900

    #100 Fix: 플랫폼 글 변환 동시에 수행하도록 수정

--- src/main/java/org/zapply/product/domain/posting/service/ClovaService.java
+++ src/main/java/org/zapply/product/domain/posting/service/ClovaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.zapply.product.domain.posting.dto.request.ToneTransferRequest;
+import org.zapply.product.domain.posting.dto.response.ToneTransferResponse;
 import org.zapply.product.global.clova.dto.ClovaMessage;
 import org.zapply.product.global.clova.enuermerate.ClovaRole;
 import org.zapply.product.global.clova.enuermerate.SNSType;
@@ -15,6 +16,7 @@ import org.zapply.product.global.clova.ClovaAiClient;
 import org.zapply.product.global.clova.dto.request.ClovaRequest;
 import org.zapply.product.global.clova.dto.response.ClovaResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,31 +45,61 @@ public class ClovaService {
 
     private final ClovaAiClient clovaAiClient;
 
-    public String TransferToSNSTone(ToneTransferRequest toneTransferRequest) {
+    /**
+     * SNS 글 분위기 변환
+     * @param toneTransferRequest 사용자 입력 콘텐츠와 SNS 타입을 포함하는 요청 객체
+     * @return 변환된 콘텐츠를 포함하는 ToneTransferResponse 객체
+     * @param toneTransferRequest
+     * @return ToneTransferResponse 객체
+     */
+    public ToneTransferResponse TransferToSNSTone(ToneTransferRequest toneTransferRequest) {
         String authorizationHeader = "Bearer " + apiKey;
-        SNSType snsType = toneTransferRequest.snsType();
+        List<SNSType> snsTypes = toneTransferRequest.snsTypes();
         String userPrompt = toneTransferRequest.userPrompt();
 
-        String systemPrompt;
-        switch (snsType) {
-            case THREADS    -> systemPrompt = threadsPrompt;
-            case INSTAGRAM  -> systemPrompt = instagramPrompt;
-            case FACEBOOK   -> systemPrompt = facebookPrompt;
-            default         -> throw new CoreException(GlobalErrorType.CLOVA_API_ERROR);
+        List<ToneTransferResponse.ToneTransferResponseItem> responseItems = new ArrayList<>();
+
+        for (SNSType snsType : snsTypes) {
+            String systemPrompt;
+
+            // snsType별로 정확하게 매칭
+            switch (snsType) {
+                case THREADS -> systemPrompt = threadsPrompt;
+                case INSTAGRAM -> systemPrompt = instagramPrompt;
+                case FACEBOOK -> systemPrompt = facebookPrompt;
+                default -> throw new CoreException(GlobalErrorType.SNS_TYPE_NOT_FOUND);
+            }
+
+            try {
+                ClovaRequest clovaRequest = ClovaRequest.from(List.of(
+                        ClovaMessage.of(ClovaRole.SYSTEM.getValue(), systemPrompt),
+                        ClovaMessage.of(ClovaRole.USER.getValue(), userPrompt)
+                ));
+                ClovaResponse clovaResponse = clovaAiClient.getCompletions(authorizationHeader, modelName, clovaRequest);
+                String content = clovaResponse.result().message().content();
+
+                ToneTransferResponse.ToneTransferResponseItem responseItem =
+                        ToneTransferResponse.ToneTransferResponseItem.builder()
+                                .snsType(snsType)
+                                .content(content)
+                                .build();
+
+                responseItems.add(responseItem);
+            } catch (Exception e) {
+                throw new CoreException(GlobalErrorType.CLOVA_API_ERROR);
+            }
         }
 
-        try{
-            ClovaRequest clovaRequset = ClovaRequest.from(List.of(
-                    ClovaMessage.of(ClovaRole.SYSTEM.getValue(), systemPrompt),
-                    ClovaMessage.of(ClovaRole.USER.getValue(),   userPrompt)
-            ));
-            ClovaResponse clovaResponse = clovaAiClient.getCompletions(authorizationHeader, modelName, clovaRequset);
-            return clovaResponse.result().message().content();
-        } catch (Exception e) {
-            throw new CoreException(GlobalErrorType.CLOVA_API_ERROR);
-        }
+        return ToneTransferResponse.builder()
+                .toneTransferResponseItems(responseItems)
+                .build();
     }
 
+    /**
+     * 글에 맞는 컨텐츠 제목 추천
+     * @param toneTransferRequest 사용자 입력 콘텐츠를 포함하는 요청 객체
+     * @return 추천된 제목 문자열
+     */
     public String recommendProjectTitle(ToneTransferRequest toneTransferRequest) {
         String authorizationHeader = "Bearer " + apiKey;
         String userPrompt = toneTransferRequest.userPrompt();
